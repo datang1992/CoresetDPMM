29857c81d6c72183030501d4e2137402590b811e
datang1992 <qltedxtc@gmail.com>
2015-04-18 19:51:07 +0800

    Add header file for the SVA model.

--- model_SVA.h
+++ model_SVA.h
@@ -2,8 +2,45 @@
 #define __MODEL_SVA_H__
 
 
+#include <vector>
+#include <math.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_matrix.h>
+
+using namespace std;
 
 
+class SVA_model {
+	public:
+		int K,  N;
+		int dim;
+		vector<double> y;
+		vector<gsl_matirx*> x;
+		vector<int> z;
+		vectore<double> zeta;
+		vector<int> Num;
+		vector<gsl_matrix*> eta;
+		
+		int initial_cluster_number;
+		
+		int Nthreads;
+
+		int number_of_iterations;
+		
+		vector<double> omega;
+
+		const double C, l, nu;
+		const double alpha;
+
+		long rand_seed;
+		gsl_rng *rng;
+
+
+		void initialize();
+		
+		SVA_model();
+		~SVA_model();
+};
 
 
 #endif
