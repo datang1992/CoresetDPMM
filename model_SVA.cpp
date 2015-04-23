e5050f55c7a9ccb22f574d5c81bd28d190bddff4
datang1992 <qltedxtc@gmail.com>
2015-04-23 20:52:43 +0800

    Add model_SVA.cpp

---
+++ model_SVA.cpp
@@ -0,0 +1,38 @@
+#include "model_SVA.h"
+
+using namespace std;
+using namespace Eigen;
+
+SVA_model::SVA_model(int K, int N, int dim, double C, double l, double nu, double alpha): K(K), N(N), dim(dim), C(C), l(l), nu(nu), alpha(alpha) {
+	
+}
+
+SVA_model::~SVA_model() {
+
+}
+
+void SVA_model::initialize() {
+
+}
+
+void SVA_model::find_bacteria_solution() {
+
+}
+
+void SVA_model::compute_coreset() {
+
+}
+
+void SVA_model::M2DPM() {
+
+}
+
+void SVA_model::map_back() {
+
+}
+
+void SVA_model::cross_validiction() {
+
+}
+
+
