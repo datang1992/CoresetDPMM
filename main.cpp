5ecdba501191ceff46bac165a0fa99010e3e8275
datang1992 <qltedxtc@gmail.com>
2015-05-24 21:09:52 +0800

    Tuned some parameters.

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@
 using namespace std;
 
 int main () {
-	int N = 100000;
+	int N = 10000;
 	double K = 1;
 	int dim = 2;
-	double C = 1, l = 1, S = 1, nu = 1, nu2 = 1;
+	double C = 1, l = 10000, S = 0.01, nu = 0.1, nu2 = 0.1;
 	double lambda = 1;
 	double SVM_learning_rate = 0.2;
 	double SVM_iterations = 1000;
 	double coreset_epsilon = 1e-2;
 	double omega_min = 1e-3;
 	int initial_cluster_number = 10;
-	int number_of_iterations = 1000;
+	int number_of_iterations = 100;
 	SVA_model model(K, N, dim, C, l, S, nu, nu2, lambda, SVM_learning_rate, SVM_iterations, coreset_epsilon, initial_cluster_number, number_of_iterations, omega_min);
 	for (int i = 0; i < N; i++) {
 		Vector2d v;
-		v(0) = gsl_ran_gaussian(model.rng, 1);
-		v(1) = gsl_ran_gaussian(model.rng, 1);
+		v(0) = i / 10000.0 - 0.5;//gsl_ran_gaussian(model.rng, 1);
+		v(1) = (i % 100) / 100.0 - 0.5;//gsl_ran_gaussian(model.rng, 1);
 		model.x.push_back(v);
 		if (v(0) + v(1) > 0)
 			model.y.push_back(1);
@@ -45,9 +45,13 @@ int main () {
 	memset(coreset_size, 0, sizeof(int) * model.mu.size());
 	for (int i = 0; i < N; i++)
 		coreset_size[model.z[i]]++;
+	int used_clusters = 0;
 	for (int i = 0; (unsigned int) i < model.mu.size(); i++)
-		if (coreset_size[i] > 0)
-			cout << i << '\t' << coreset_size[i] << '\t' << setiosflags(ios::fixed) << setprecision(6) << model.eta[i](0) << '\t' << model.eta[i](1) << '\t' << endl;
+		if (coreset_size[i] > 0) {
+			cout << i << '\t' << coreset_size[i] << '\t' << setiosflags(ios::fixed) << setprecision(6) << model.eta[i](0) << '\t' << model.eta[i](1) << '\t' << model.mu[i](0) << '\t' << model.mu[i](1) << endl;
+			used_clusters++;
+		}
+	cout << "Used clusters: " << used_clusters << endl;
 	delete[] coreset_size;
 	return 0;
 }
